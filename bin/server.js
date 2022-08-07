e8d3b85d45ded297d54a55370ac07fe3eb28fa9a
Plínio Augusto <plinioaugustoadm@hotmail.com>
2022-08-07 03:01:03 -0300

    refactor: :recycle: translating some terms

--- bin/server.js
+++ bin/server.js
@@ -1,59 +1,54 @@
-'use strict'
+"use strict";
 
-const app = require('../src/app');
-const http = require('http');
-const debug = require('debug')('querodelivery:server');
+const app = require("../src/app");
+const http = require("http");
+const debug = require("debug")("querodelivery:server");
 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
-//Criando servidor
 const server = http.createServer(app);
 
-//Servidor ouvindo a porta
 server.listen(port);
-server.on('error', onError);
-server.on('Ouvindo ', onListening);
-console.log('API rodando na porta ' + port);
+server.on("error", onError);
+server.on("listening ", onListening);
+console.log("API running on port " + port);
 
-//Normalizando a porta
-function normalizePort(val){
-    const port = parseInt(val, 10);
+function normalizePort(val) {
+  const port = parseInt(val, 10);
 
-    if(isNaN(port)){
-        return val;
-    }
-    if(port >= 0){
-        return port;
-    }
-    return false;
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
 }
 
-//Gerenciando erros
-function onError(error){
-    if(error.syscall !== 'listen'){
-        throw error;
-    } 
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
 
-    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port; 
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
-    switch (error.code){
-        case 'EACCES':
-            console.error(bind + ' requer privilégios elevados');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' já está em uso');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
-//Iniciando o Debug
-function onListening(){
-    const addr = server.address();
-    const bind = typeof addr === 'sting' ? 'pipe ' + addr : 'port ' + addr.port;
-    debug('Ouvindo em ' + bind);
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "sting" ? "pipe " + addr : "port " + addr.port;
+  debug("Ouvindo em " + bind);
 }
--- src/controllers/categoria-controller.js
+++ src/controllers/categoria-controller.js
@@ -1,62 +1,62 @@
-'use strict';
+"use strict";
 
-const repositorio = require('../repositories/categoria-repositorie');
+const repository = require("../repositories/categoria-repositorie");
 
-exports.get = async(req, res, next) =>{
-    try{
-        var data = await repositorio.get();
-        res.status(200).send(data);
-    }catch(e){
-        res.status(500).send({
-            message: "Falha ao processar sua requisição"
-        });
-    }
+exports.get = async (req, res, next) => {
+  try {
+    const data = await repository.get();
+    res.status(200).send(data);
+  } catch (e) {
+    res.status(500).send({
+      message: "Failed to process your request",
+    });
+  }
 };
 
-exports.getById = async(req, res, next) =>{
-    try{
-        var data = await repositorio.getById(req.params.id);
-        res.status(200).send(data);
-    }catch(e){
-        res.status(500).send({
-            message: "Falha ao processar sua requisição"
-        });
-    }
-}
-
-exports.post = async(req, res, next) => {
-    try{
-        await repositorio.post(req.body);
-        res.status(201).send({message: 'Categoria cadastrada com sucesso!'});
-    }catch(e){
-        res.status(500).send({
-            message: "Falha ao processar sua requisição"
-        });
-    }
+exports.getById = async (req, res, next) => {
+  try {
+    const data = await repository.getById(req.params.id);
+    res.status(200).send(data);
+  } catch (e) {
+    res.status(500).send({
+      message: "Failed to process your request",
+    });
+  }
 };
 
-exports.put = async(req, res, next) => {
-    try{
-        await repositorio.put(req.params.id, req.body);
-        res.status(200).send({
-            message: 'Categoria atualizada com sucesso!'
-        });
-    }catch(e){
-        res.status(500).send({
-            message: "Falha ao processar sua requisição"
-        });
-    }
+exports.post = async (req, res, next) => {
+  try {
+    await repository.post(req.body);
+    res.status(201).send({ message: "Category registered successfully!" });
+  } catch (e) {
+    res.status(500).send({
+      message: "Failed to process your request",
+    });
+  }
 };
 
-exports.delete = async(req, res, next) => {
-    try{
-        await repositorio.delete(req.body.id);
-        res.status(200).send({
-            message: 'Categoria removida com sucesso!'
-        });
-    }catch(e){
-        res.status(500).send({
-            message: "Falha ao processar sua requisição"
-        });
-    }
+exports.put = async (req, res, next) => {
+  try {
+    await repository.put(req.params.id, req.body);
+    res.status(200).send({
+      message: "Category updated successfully!",
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: "Failed to process your request",
+    });
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  try {
+    await repository.delete(req.body.id);
+    res.status(200).send({
+      message: "Category removed successfully!",
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: "Failed to process your request",
+    });
+  }
 };
